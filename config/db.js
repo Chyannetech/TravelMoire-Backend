10ec47051819ac4455b67fbcc05220f069474fec
Chyanne Robbins <chyannetech@gmail.com>
2022-08-14 23:40:37 -0400

    Database Connected

---
+++ config/db.js
@@ -0,0 +1,14 @@
+//connect mongoose db
+const mongoose = require('mongoose');
+
+const connectDB = async () => { 
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+module.exports = connectDB
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const connectDB = require("./config/db")
 const app = express();
 
-const port = 3000;
+const { PORT = 8000} = process.env || 8000;
 
-app.listen(port, () => {
-    console.log(`Hello World`)
-})
+//database connection
+connectDB()
+
+//middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// Express Listener
+app.listen( PORT, () => console.log(`Express is listening on:, port ${PORT}`));
+
+
+
