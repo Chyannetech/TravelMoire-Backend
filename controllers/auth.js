2591572f3207c69127d546ca9d5955e1577c09f7
Chyanne Robbins <chyannetech@gmail.com>
2022-09-30 03:08:24 -0400

    Comment-out JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,133 +1,133 @@
-const User = require("../models/User");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+// const User = require("../models/User");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
 
-const { createJWT } = require("../utils/auth");
+// const { createJWT } = require("../utils/auth");
 
-//email validation expressions
-const emailRegexp =
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+// //email validation expressions
+// const emailRegexp =
+//   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-//user signup auth
-exports.signup = (req, res, next) => {
-  let { email, password } = req.body;
-  let errors = [];
-  if (!email) {
-    errors.push({ email: "required" });
-  }
-  if (!emailRegexp.test(email)) {
-    errors.push({ email: "invalid" });
-  }
-  if (!password) {
-    errors.push({ password: "required" });
-  }
-  if (errors.length > 0) {
-    return res.status(422).json({ errors: errors });
-  }
+// //user signup auth
+// exports.signup = (req, res, next) => {
+//   let { email, password } = req.body;
+//   let errors = [];
+//   if (!email) {
+//     errors.push({ email: "required" });
+//   }
+//   if (!emailRegexp.test(email)) {
+//     errors.push({ email: "invalid" });
+//   }
+//   if (!password) {
+//     errors.push({ password: "required" });
+//   }
+//   if (errors.length > 0) {
+//     return res.status(422).json({ errors: errors });
+//   }
 
-  //check if user exists.
-  // if the user already exists throw error : email already exists.
-  //if the user is a new user, use bcrypt to hash the password before storing it in database
-  //save data(name, email, and password) in MongoDB
-  User.findOne({ email: email })
-    .then((user) => {
-      if (user) {
-        return res
-          .status(422)
-          .json({ errors: [{ user: "email already exists" }] });
-      } else {
-        const user = new User({
-          email: email,
-          password: password,
-        });
-        bcrypt.genSalt(10, function (err, salt) {
-          bcrypt.hash(password, salt, function (err, hash) {
-            if (err) throw err;
-            user.password = hash;
-            user
-              .save()
-              .then((response) => {
-                res.status(200).json({
-                  success: true,
-                  result: response,
-                });
-              })
-              .catch((err) => {
-                res.status(500).json({
-                  errors: [{ error: err }],
-                });
-              });
-          });
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        errors: [{ error: "Something went wrong" }],
-      });
-    });
-};
+//   //check if user exists.
+//   // if the user already exists throw error : email already exists.
+//   //if the user is a new user, use bcrypt to hash the password before storing it in database
+//   //save data(name, email, and password) in MongoDB
+//   User.findOne({ email: email })
+//     .then((user) => {
+//       if (user) {
+//         return res
+//           .status(422)
+//           .json({ errors: [{ user: "email already exists" }] });
+//       } else {
+//         const user = new User({
+//           email: email,
+//           password: password,
+//         });
+//         bcrypt.genSalt(10, function (err, salt) {
+//           bcrypt.hash(password, salt, function (err, hash) {
+//             if (err) throw err;
+//             user.password = hash;
+//             user
+//               .save()
+//               .then((response) => {
+//                 res.status(200).json({
+//                   success: true,
+//                   result: response,
+//                 });
+//               })
+//               .catch((err) => {
+//                 res.status(500).json({
+//                   errors: [{ error: err }],
+//                 });
+//               });
+//           });
+//         });
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(500).json({
+//         errors: [{ error: "Something went wrong" }],
+//       });
+//     });
+// };
 
-//user signin auth
-exports.signin = (req, res) => {
-  let { email, password } = req.body;
-  let errors = [];
-  if (!email) {
-    errors.push({ email: "required" });
-  }
-  if (!emailRegexp.test(email)) {
-    errors.push({ email: "invalid email" });
-  }
-  if (!password) {
-    errors.push({ password: "required" });
-  }
-  if (errors.length > 0) {
-    return res.status(422).json({ errors: errors });
-  }
+// //user signin auth
+// exports.signin = (req, res) => {
+//   let { email, password } = req.body;
+//   let errors = [];
+//   if (!email) {
+//     errors.push({ email: "required" });
+//   }
+//   if (!emailRegexp.test(email)) {
+//     errors.push({ email: "invalid email" });
+//   }
+//   if (!password) {
+//     errors.push({ password: "required" });
+//   }
+//   if (errors.length > 0) {
+//     return res.status(422).json({ errors: errors });
+//   }
 
-  //check if the user exists, if not throw error user not found.
-  //if the user exists, check whether the assigned and retrieved passwords are the same or not using the bcrypt.compare() method.
-  //sign our jwt and set the JWT token expiration time.
-  //if succeed send the token in response with success status(200) and user information.
-  User.findOne({ email: email })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json({
-          errors: [{ user: "not found" }],
-        });
-      } else {
-        bcrypt
-          .compare(password, user.password)
-          .then((isMatch) => {
-            if (!isMatch) {
-              return res
-                .status(400)
-                .json({ errors: [{ password: "incorrect" }] });
-            }
-            let access_token = createJWT(user.email, user._id, 3600);
-            jwt.verify(
-              access_token,
-              process.env.TOKEN_SECRET,
-              (err, decoded) => {
-                if (err) {
-                  res.status(500).json({ erros: err });
-                }
-                if (decoded) {
-                  return res.status(200).json({
-                    success: true,
-                    token: access_token,
-                    message: user,
-                  });
-                }
-              }
-            );
-          })
-          .catch((err) => {
-            res.status(500).json({ erros: err });
-          });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ erros: err });
-    });
-};
+//   //check if the user exists, if not throw error user not found.
+//   //if the user exists, check whether the assigned and retrieved passwords are the same or not using the bcrypt.compare() method.
+//   //sign our jwt and set the JWT token expiration time.
+//   //if succeed send the token in response with success status(200) and user information.
+//   User.findOne({ email: email })
+//     .then((user) => {
+//       if (!user) {
+//         return res.status(404).json({
+//           errors: [{ user: "not found" }],
+//         });
+//       } else {
+//         bcrypt
+//           .compare(password, user.password)
+//           .then((isMatch) => {
+//             if (!isMatch) {
+//               return res
+//                 .status(400)
+//                 .json({ errors: [{ password: "incorrect" }] });
+//             }
+//             let access_token = createJWT(user.email, user._id, 3600);
+//             jwt.verify(
+//               access_token,
+//               process.env.TOKEN_SECRET,
+//               (err, decoded) => {
+//                 if (err) {
+//                   res.status(500).json({ erros: err });
+//                 }
+//                 if (decoded) {
+//                   return res.status(200).json({
+//                     success: true,
+//                     token: access_token,
+//                     message: user,
+//                   });
+//                 }
+//               }
+//             );
+//           })
+//           .catch((err) => {
+//             res.status(500).json({ erros: err });
+//           });
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ erros: err });
+//     });
+// };
--- utils/auth.js
+++ utils/auth.js
@@ -1,13 +1,13 @@
 //sign jwt token -- payload, expiry time, and token secret.
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-exports.createJWT = (email, userId, duration) => {
-   const payload = {
-      email,
-      userId,
-      duration
-   };
-   return jwt.sign(payload, process.env.TOKEN_SECRET, {
-     expiresIn: duration,
-   });
-};
+// exports.createJWT = (email, userId, duration) => {
+//    const payload = {
+//       email,
+//       userId,
+//       duration
+//    };
+//    return jwt.sign(payload, process.env.TOKEN_SECRET, {
+//      expiresIn: duration,
+//    });
+// };
